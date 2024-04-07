14f6d21077d184d28e97409c5cc7b4a107a9a7b1
Marmik Thakkar <marmik18@gmail.com>
2024-04-07 09:25:48 +0000

    updated docker run

--- docker/run.sh
+++ docker/run.sh
@@ -8,3 +8,9 @@ python -m pcdet.datasets.custom.custom_dataset create_custom_infos tools/cfgs/da
 # Run the training
 cd tools
 python train.py --cfg_file cfgs/models/custom_models/TED-S.yaml
+
+# Run the evaluation
+python test.py --cfg_file cfgs/models/custom_models/TED-S.yaml
+
+# Run the visualization
+python demo.py --cfg_file cfgs/models/custom_models/TED-S.yaml --ckpt ../output/TED-S/default/ckpt/checkpoint_epoch_80.pth --data_path ../data/custom_dataset/points
