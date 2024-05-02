19a2c354d3c47c0f8bfb26e1efdc3fef702eafc7
Marmik Thakkar <marmik18@gmail.com>
2024-05-03 00:21:06 +0200

    moved to training folder

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++
@@ -1,7 +0,0 @@
-from .height_compression import BEVPool
-from .pointpillar_scatter import PointPillarScatter
-
-__all__ = {
-    'BEVPool': BEVPool,
-    'PointPillarScatter': PointPillarScatter
-}
--- pcdet/models/roi_heads/__init__.py
+++
@@ -1,9 +0,0 @@
-from .roi_head_template import RoIHeadTemplate
-
-from .ted_head import TEDMHead, TEDSHead
-
-__all__ = {
-    'RoIHeadTemplate': RoIHeadTemplate,
-    'TEDSHead': TEDSHead,
-    'TEDMHead': TEDMHead
-}
