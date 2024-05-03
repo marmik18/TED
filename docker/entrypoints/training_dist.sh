a421c9586622be4b6841bee16b75f31c8b155bed
Marmik Thakkar <marmik18@gmail.com>
2024-05-04 00:34:25 +0200

    added support for dist train

---
+++ docker/entrypoints/training_dist.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -c [path-to-config-file]"
+    echo "Options:"
+    echo -e "\t-c: Path to the config file"
+    exit 1
+}
+
+
+while getopts "c:" opt; do
+    case $opt in
+        c) CONFIG_PATH=$OPTARG;;
+        *) helpFunction ;;
+      esac
+done
+
+if [ -z "$CONFIG_PATH" ]; then
+    echo "Please provide the path to the config file"
+    helpFunction
+fi
+
+# Install the requirements
+python setup.py develop
+
+# Run the script to create dataset infos
+python -m pcdet.datasets.custom.custom_dataset create_custom_infos tools/cfgs/dataset_configs/custom_dataset.yaml
+
+# Run the training
+cd tools
+./dist_train.sh --cfg_file $CONFIG_PATH
--- training/tools/dist_test.sh
+++ training/tools/dist_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-CUDA_VISIBLE_DEVICES=1,2,3,4 nohup python3 -m torch.distributed.launch --nproc_per_node=4 test.py --launcher pytorch > log-test.txt &
+PY_ARGS=${@:1}
+CUDA_VISIBLE_DEVICES=1,2,3,4 nohup python3 -m torch.distributed.launch --nproc_per_node=4 test.py --launcher pytorch ${PY_ARGS} > log-test.txt &
 
