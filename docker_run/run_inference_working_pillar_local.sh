c87dd777abf282a7aeb802e5e8fff4e22d720000
Marmik Thakkar <marmik18@gmail.com>
2024-05-12 20:52:00 +0200

    setting ROS_LOCALHOST_ONLY 1

--- docker_run/run_inference_working_pillar_local.sh
+++ docker_run/run_inference_working_pillar_local.sh
@@ -3,10 +3,12 @@
 DIR=$(dirname $(realpath $0))
 ROS_DOMAIN_ID=30
 RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
+ROS_LOCALHOST_ONLY=1
 
 docker run -it --rm --runtime nvidia --gpus all --net=host \
     -v $DIR/../training/build:/root/OpenPCDet/build \
     -v $DIR/../training/output:/root/OpenPCDet/output \
+    -e ROS_LOCALHOST_ONLY=$ROS_LOCALHOST_ONLY \
     -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
     -e RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION \
     wzl/point-cloud-pose-estimation-inference:latest \
